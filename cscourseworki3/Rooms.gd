9324965cf57b33454296f96a921e44de83d8d8fc
tahyat <tahyat.azhary21@gmail.com>
2025-02-11 01:31:51 +0000

    tilemap set up
    rooms are indeed working, they require a Marker2D and Position2D2 under the Entrance Node, a Door node leading to the top of the frame, and a CollisionShape2D under the playerdetector. furniture tileamp for futnutre, tilemap1 for collidables (walls, obstacles), tilemap 2 for the coving/banister things (deco)

--- cscourseworki3/Rooms.gd
+++ cscourseworki3/Rooms.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const SPAWN_ROOMS: Array = [preload("res://Rooms/SpawnRoom0.tscn"), preload("res://Rooms/SpawnRoom1.tscn")]
-const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn"), preload("res://Rooms/Room1.tscn"), preload("res://Rooms/Room2.tscn"), preload("res://Rooms/Room3.tscn"), preload("res://Rooms/Room4.tscn")]
+const SPAWN_ROOMS: Array = [preload("res://Rooms/SpawnRoom0.tscn")]
+const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn")]
 const SPECIAL_ROOMS: Array = [preload("res://Rooms/SpecialRoom0.tscn"), preload("res://Rooms/SpecialRoom1.tscn")]
 const END_ROOMS: Array = [preload("res://Rooms/EndRoom0.tscn")]
 const SLIME_BOSS_SCENE: PackedScene = preload("res://Rooms/SlimeBossRoom.tscn")
