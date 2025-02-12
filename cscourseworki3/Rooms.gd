169879f43d275cba42a7c47ca3bf7af7d7e5f8c9
tahyat <tahyat.azhary21@gmail.com>
2025-02-12 00:29:38 +0000

    rooms
    added 5 main rooms, just add tutorial and you're good to go! (need to fix slime boss he's stupid rn)

--- cscourseworki3/Rooms.gd
+++ cscourseworki3/Rooms.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 const SPAWN_ROOMS: Array = [preload("res://Rooms/SpawnRoom0.tscn")]
-const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn")]
-const SPECIAL_ROOMS: Array = [preload("res://Rooms/SpecialRoom0.tscn"), preload("res://Rooms/SpecialRoom1.tscn")]
+const INTERMEDIATE_ROOMS: Array = [preload("res://Rooms/Room0.tscn"),preload("res://Rooms/Room1.tscn"),preload("res://Rooms/Room2.tscn"),preload("res://Rooms/Room3.tscn"),preload("res://Rooms/Room4.tscn"),preload("res://Rooms/Room5.tscn")]
+const SPECIAL_ROOMS: Array = [preload("res://Rooms/SpecialRoom0.tscn"),preload("res://Rooms/SpecialRoom1.tscn")]
 const END_ROOMS: Array = [preload("res://Rooms/EndRoom0.tscn")]
 const SLIME_BOSS_SCENE: PackedScene = preload("res://Rooms/SlimeBossRoom.tscn")
 
@@ -35,7 +35,7 @@ func _spawn_rooms() -> void:
 			player.position = room.get_node("PlayerSpawnPos").position
 		else:
 			if i == num_levels - 1:
-				room = END_ROOMS[randi() % END_ROOMS.size()].instantiate()
+				room =  END_ROOMS.pick_random().instantiate()
 			else:
 				if SavedData.num_floor == 3:
 					room = SLIME_BOSS_SCENE.instantiate()
@@ -56,9 +56,10 @@ func _spawn_rooms() -> void:
 				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2i(-1, -y), FLOOR_TILE_INDEX, Vector2i.ZERO)
 				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2i(0, -y), FLOOR_TILE_INDEX, Vector2i.ZERO)
 				previous_room_tilemap.set_cell(0, exit_tile_pos + Vector2i(1, -y), RIGHT_WALL_TILE_INDEX, Vector2i.ZERO)
-
+			
 			var room_tilemap: TileMap = room.get_node("TileMap")
-			room.position = previous_room_door.global_position + Vector2.UP * room_tilemap.get_used_rect().size.y * TILE_SIZE + Vector2.UP * (1 + corridor_height) * TILE_SIZE + Vector2.LEFT * room_tilemap.local_to_map(room.get_node("Entrance/Position2D2").position).x * TILE_SIZE
+			room.position = previous_room_door.global_position + Vector2.UP * (room_tilemap.get_used_rect().size.y + corridor_height + 1) * TILE_SIZE + Vector2.LEFT * room_tilemap.local_to_map(room.get_node("Entrance/Marker2D2").position).x * TILE_SIZE
+				
 
 		add_child(room)
 		previous_room = room
