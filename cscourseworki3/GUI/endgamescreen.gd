abaf8f99cf5d7645bc4a3348fdab613c26bdea4c
tahyat <tahyat.azhary21@gmail.com>
2025-02-24 00:16:07 +0000

    gui update
    added diff ui screens

--- cscourseworki3/GUI/endgamescreen.gd
+++ cscourseworki3/GUI/endgamescreen.gd
@@ -8,3 +8,7 @@ func _on_back_to_menu_pressed() -> void:
 
 func _on_continue_pressed() -> void:
 	SceneTransistor.start_transition_to("res://Game.tscn")
+
+func _on_replay_pressed() -> void:
+	SavedData.reset_data()
+	SceneTransistor.start_transition_to("res://Game.tscn")
